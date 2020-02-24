f51e7d7bb034850ededf3824b73e16fc9eb68e86
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-23 20:51:22 -0600

    Imports, Variables y Fuentes de Google en Sass

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -6,7 +6,7 @@ import plusIcon from '../assets/static/plus-icon.png';
 const CarouselItem = () => {
   return (
     <div className='carousel-item'>
-      <img className='carousel-item__img' src='https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260' alt />
+      <img className='carousel-item__img' src='https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=750&w=1260' />
       <div className='carousel-item__details'>
         <div>
           <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
