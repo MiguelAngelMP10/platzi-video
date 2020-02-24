fb178e197ef4e89dbf65ed3f3fcac34f8838a5bd
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-23 18:58:14 -0600

    Añadimos Header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import HelloWorld from "./components/HelloWorld";
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
