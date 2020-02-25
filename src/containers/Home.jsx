3727945b43a8615c6eac8ecd6c3f4b82914ca280
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-24 21:50:04 -0600

    Crear nuestro archivo de Rutas

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
 
   const initilState = useInitilState(API);
   const { mylist, trends, originals } = initilState;
@@ -46,4 +46,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
