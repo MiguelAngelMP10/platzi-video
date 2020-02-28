5bce9d7a496568edb38cfcdbbe255279a475685f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-02-27 20:58:59 -0600

    Creando el Store de Redux

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,28 +1,22 @@
 import React, { useState, useEffect } from 'react';
-
+import { connect } from 'react-redux';
 import Search from '../components/Search';
-
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitilState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initialState';
 
-const Home = () => {
-
-  const initilState = useInitilState(API);
-  const { mylist, trends, originals } = initilState;
+const Home = ({ myList, trends, originals }) => {
 
   return (
     <>
       <Search />
 
-      {mylist !== undefined && mylist.length > 0 && (
+      {myList !== undefined && myList.length > 0 && (
         <Categories title='Mi Lista'>
           <Carousel>
-            {mylist !== undefined && mylist.map((item) => <CarouselItem key={item.id} {...item} />)}
+            {myList !== undefined && myList.map((item) => <CarouselItem key={item.id} {...item} />)}
           </Carousel>
         </Categories>
       )}
@@ -43,4 +37,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
