131f729ab4a7f97b319736216a8d6ac990467330
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-03-04 18:51:05 -0600

    Arreglando la funcionalidad del player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { setFavorite, deleteFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
