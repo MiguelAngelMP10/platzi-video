c12cafe7e9060025a63504806c412ec3a33514de
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-03-07 09:11:54 -0600

    Integración de Webpack con Express

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -9,6 +11,16 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('development config');
+
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: PORT, hot: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+
 }
 app.get('*', (req, res) => {
   res.send({
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/frontend/index.js',
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true',
+  ],
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -50,6 +54,7 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html',
