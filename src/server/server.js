a98bc8b750f3cc4e93f8db49d7f35704d854b906
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-03-07 11:34:49 -0600

    Abstrayendo React Router, creando history y haciendo initialState más accesible

--- src/server/server.js
+++ src/server/server.js
@@ -32,7 +32,6 @@ app.get('*', (req, res) => {
                   <link rel="stylesheet" href="assets/app.css" type="text/css">
                   <title>Platzi Video</title>
               </head>
-              
               <body>
                   <div id="app"></div>
                   <script src="assets/app.js" type="text/javascript"></script>
